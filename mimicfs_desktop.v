0058cbd04ad8fa87ce6b0137d8e84496a28b91ac
tailsmails <253711812+tailsmails@users.noreply.github.com>
2026-07-17 00:26:26 +0430

    Clean up comments in mimicfs_desktop.v
    Removed licensing comments and build instructions.

--- mimicfs_desktop.v
+++ mimicfs_desktop.v
@@ -1,18 +1,3 @@
-//    This program is free software: you can redistribute it and/or modify
-//    it under the terms of the GNU General Public License as published by
-//    the Free Software Foundation, either version 3 of the License, or
-//    (at your option) any later version.
-//
-//    This program is distributed in the hope that it will be useful,
-//    but WITHOUT ANY WARRANTY; without even the implied warranty of
-//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//    GNU General Public License for more details.
-//
-//    You should have received a copy of the GNU General Public License
-//    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-// v -gc boehm -prod -prealloc -skip-unused -cflags "-fstack-protector-all -Wl,-z,relro -Wl,-z,now -fPIE -pie -D_FORTIFY_SOURCE=2 -fno-stack-check -fno-unwind-tables -fno-asynchronous-unwind-tables -Wl,--build-id=none -Wl,--gc-sections" mimicfs_desktop.v -o mimicfs_desktop && strip --strip-all --remove-section=.comment --remove-section=.note --remove-section=.gnu.version mimicfs_desktop
-
 import os
 import term
 
